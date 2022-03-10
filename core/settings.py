821d2a154bb5e40f523242851dd5ddca0ffb21f9
Vitaliy-Steffensen <vitaliysteffensen@gmail.com>
2022-03-10 16:23:40 +0100

    this might work

--- core/settings.py
+++ core/settings.py
@@ -141,36 +141,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/ 
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-# #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/" 
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static','media-root')
-
-
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 
 
 

da2d06b07cf10825d31ecebec8a1ff923034d223
Vitaliy-Steffensen <vitaliysteffensen@gmail.com>
2022-03-10 14:15:19 +0100

    fsd

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x*x-+8=wyti7)c7p88&^)n07^m^!%^(b(34a@*1dn^8nok)09#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','amazon-redesign.herokuapp.com']
 

6e8df37521ac1642d551dc4dcb73895f251e740f
Vitaliy-Steffensen <vitaliysteffensen@gmail.com>
2022-03-10 21:43:27 +0100

    fsd

--- core/settings.py
+++ core/settings.py
@@ -145,17 +145,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+# STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/" 
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static','media-root')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static','media-root')
 
 
 
